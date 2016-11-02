b31012608f69cb8fc59258841a91bfefd18c7e2b
Ryan Frazier <frazierr2@gmail.com>
2016-11-02 14:18:50 -0400

    Initial Commit

---
+++ app/scripts/index.js
@@ -0,0 +1,41 @@
+var $ = require('jquery');
+
+// Setting up the Parse Server with Application Key and Rest Key to talk to server
+$.ajaxSetup({
+  beforeSend: function(xhr){
+    xhr.setRequestHeader('X-Parse-Application-Id','ryansparseserver');
+    xhr.setRequestHeader('X-Parse-REST-API-Key','ryansapikey');
+  }
+});
+// Pulling in the Server to the document
+var url = 'https://thefraz.herokuapp.com/';
+var resultPromise = $.ajax(url).then(function(data){
+  console.log(data);
+});
+
+
+// $('#signup').on('submit',function(e){
+//   e.preventDefault();
+//
+// var data = {
+//   'username': $('#new-username').val(),
+//   'password': $('#new-password').val(),
+// };
+//
+//   $.post('https://tiny-parse-server.herokuapp.com/' + 'users', data).then(function(response){
+//     console.log(response);
+//   });
+// });
+//
+//
+//
+// $('#login').on('sumbit',function(e){
+//   e.preventDefault();
+//
+//   var username = $('#signin').val();
+//   var password = $('#signin-password').val();
+//
+//   $.get('https://tiny-parse-server.herokuapp.com/'+ 'login?username=' +  username + '&password=' + password).then(function(response){
+//     console.log(response);
+//   });
+// });
