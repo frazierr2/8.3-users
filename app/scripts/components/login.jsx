0405949889566dacbc66217cc5b952ebd4368a79
Ryan Frazier <frazierr2@gmail.com>
2016-11-05 13:58:10 -0400

    changes to login and got local storage set

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -8,51 +8,48 @@ var UsersCollection = require('../models/user').UsersCollection;
 
 var Login = React.createClass({
   getInitialState: function(){
-    var username = '';
-    var password = '';
-    return {
-      username: username,
-      password: password
-    };
-  },
-  setUsername: function(e){
-    var username = e.target.value;
-    this.setState({username: username});
-    console.log(username);
-  },
-  setPassword: function(e){
-    var password = e.target.value;
-    this.setState({password: password});
-    console.log(password);
-  },
+      var email = '';
+      var password = '';
+      return {
+        email: email,
+        password: password
+      }
+    },
 
-  setLogin: function(e){
-    e.preventDefault();
-    var router = this.props.router;
-    router.navigate('chat/',{trigger: true});
+    handleEmail: function(e){
+       var userEmail = e.target.value;
+       this.setState({email: userEmail});
+     },
+     handlePassword: function(e){
+       var userPassword = e.target.value;
+       this.setState({password: userPassword});
+     },
 
-    var userData = {
-      username: this.state.username,
-      password: this.state.password
-    };
-    this.props.setLogin(userData)
-  },
+     handleSubmit: function(e){
+       e.preventDefault();
+
+       var userInfo = {
+         username: this.state.email,
+         password: this.state.password
+       };
+       this.props.handleSubmit(userInfo);
+     },
 
     render: function(){
       var self = this;
       return (
-          <div onSubmit={self.setLogin} className="col-md-6">
+          <div className="col-md-6">
             <h1>Please login</h1>
-            <form id="login">
+            <form id="login" onSubmit={self.handleSubmit} >
               <div className="form-group">
                 <label htmlFor="signin">Username</label>
-                <input onChange={self.setUsername} value={self.state.username} type="text" className="form-control" id="signin" placeholder="Username" />
+                <input onChange={self.handleEmail}  type="text" className="form-control" id="signin" placeholder="Username" />
               </div>
               <div className="form-group">
                 <label htmlFor="signin-password">Password</label>
-                <input onChange={self.setPassword} value={self.state.password} type="password" className="form-control" id="signin-password" placeholder="Password" />
+                <input onChange={self.handlePassword}  type="password" className="form-control" id="signin-password" placeholder="Password" />
               </div>
-              <a href="#chat/" className="login-btn btn btn-primary btn-lg btn-block">Sign In</a>
+              <button type="submit" className="login-btn btn btn-primary btn-lg btn-block">Sign In</button>
             </form>
           </div>
       );
@@ -62,46 +59,49 @@ var Login = React.createClass({
 
 var SignUp = React.createClass({
   getInitialState: function(){
-    var username = '';
+    var email = '';
     var password = '';
+
     return {
-      username: username,
+      email: email,
       password: password
-    };
+    }
   },
-  setUsername: function(e){
-    var username = e.target.value;
-    this.setState({username: username});
-    console.log(username);
-  },
-
-  setPassword: function(e){
-    var password = e.target.value;
-    this.setState({password: password});
-    console.log(password);
+  handleEmail: function(e){
+    var userEmail = e.target.value;
+    this.setState({email: userEmail});
+    // console.log(userEmail);
   },
 
-  setSetup: function(e){
-    e.preventDefault();
+  handlePassword: function(e){
+  var userPassword = e.target.value;
+  this.setState({password: userPassword});
+},
 
-    var userInformation = {
-      username: this.state.username,
-      password: this.state.password
-    };
-    this.props.setSetup(userInformation)
-  },
+handleSignUp: function(e){
+  e.preventDefault();
+
+  var userData = {
+    username: this.state.email,
+    password: this.state.password
+  };
+  // console.log(userData);
+  this.props.handleSignUp(userData);
+
+},
     render: function(){
+      var self = this;
       return (
-          <div onSubmit={this.setSetup} className="col-md-6">
+          <div className="col-md-6">
             <h1>No Account? Please sign up</h1>
-            <form id="signup">
+            <form onSubmit={self.handleSignUp} id="signup">
               <div className="form-group">
                 <label htmlFor="new-username">Username</label>
-                <input onChange={this.setUsername} value={this.state.username} type="text" className="form-control" id="new-username" placeholder="Username" />
+                <input onChange={self.handleEmail} type="text" className="form-control" id="new-username" placeholder="Username" />
               </div>
               <div className="form-group">
                 <label htmlFor="new-password">Password</label>
-                <input onChange={this.setPassword} value={this.state.password} type="password" className="form-control" id="new-password" placeholder="Password" />
+                <input onChange={self.handlePassword} type="password" className="form-control" id="new-password" placeholder="Password" />
               </div>
               <button type="submit" className="btn btn-primary btn-lg btn-block">Create An Account</button>
             </form>
@@ -111,49 +111,50 @@ var SignUp = React.createClass({
 });
 
 var LoginContainer = React.createClass({
-componentWillMount: function(){
-  this.ajaxSetup();
-},
-ajaxSetup:function(token){
-  $.ajaxSetup({
-    beforeSend: function(xhr){
-      xhr.setRequestHeader('X-Parse-Application-Id', 'ryansparseserver');
-      xhr.setRequestHeader('X-Parse-REST-API-Key', 'ryansapikey');
-      if(token){
-        xhr.setRequestHeader('X-Parse-Session-Token', 'token')
+  componentWillMount: function(){
+    this.ajaxSetup();
+  },
+  ajaxSetup: function(token){
+    $.ajaxSetup({
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('X-Parse-Application-Id', 'ryansparseserver');
+        xhr.setRequestHeader('X-Parse-REST-API-Key', 'ryansapikey');
+        if(token){
+          xhr.setRequestHeader('X-Parse-Session-Token', token);
+        }
       }
-    }
-  });
-},
-  setLogin: function(savedLogin){
-
-    var username = savedLogin.username;
-    var password = savedLogin.password;
+    });
+  },
+  handleSubmit: function(userInfo){
+    var username = userInfo.username;
+    var password = userInfo.password;
+    // console.log(userInfo);
     var self = this;
     var url = 'https://thefraz.herokuapp.com/';
-
+  console.log('testing');
     $.ajax(url + 'login?username=' + username + '&password=' + password).then(function(response){
       localStorage.setItem('username', response.username);
       localStorage.setItem('token', response.sessionToken);
-      if (response.sessionToken){
+      localStorage.setItem('token', response.password);
+      if (response.sessionToken) {
         self.props.router.navigate('chat/', {trigger: true});
-      }else{
-        alert('Please Sign In!')
       };
     });
   },
 
-  setSetup: function(userData){
-    $.post('https://thefraz.herokuapp.com/users' + userData).then(function(response){
+  handleSignUp: function(userData){
+    console.log(userData);
+    $.post('https://thefraz.herokuapp.com/users', userData).then(function(response){
       console.log(response);
     });
   },
 
+
   render:function(){
     return(
       <TemplateContainer>
-        <Login />
-        <SignUp setSetup={this.state.setSetup}/>
+        <Login handleSubmit={this.handleSubmit}/>
+        <SignUp handleSignUp={this.handleSignUp}/>
       </TemplateContainer>
     );
   }
